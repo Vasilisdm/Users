8fb0274f6ff8533193caab80cb5de42fdb8a5bd7
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-03 19:30:20 +0200

    Created the Edit method.

--- Users/Controllers/AdminController.cs
+++ Users/Controllers/AdminController.cs
@@ -7,16 +7,30 @@ namespace Users.Controllers
 {
     public class AdminController : Controller
     {
-        private UserManager<AppUser> userManager;
 
-        public AdminController(UserManager<AppUser> usrMgr)
+        private UserManager<AppUser> userManager;
+        private IUserValidator<AppUser> _userValidator;
+        private IPasswordValidator<AppUser> _passwordValidator;
+        private IPasswordHasher<AppUser> _passwordHasher;
+
+
+        public AdminController(UserManager<AppUser> usrMgr,
+            IUserValidator<AppUser> userValid,
+            IPasswordValidator<AppUser> passValid,
+            IPasswordHasher<AppUser> passwordHash)
         {
             userManager = usrMgr;
+            _userValidator = userValid;
+            _passwordValidator = passValid;
+            _passwordHasher = passwordHash;
         }
+
 
         public ViewResult Index() => View(userManager.Users);
 
+
         public ViewResult Create() => View();
+
 
         [HttpPost]
         public async Task<IActionResult> Create(CreateModel model)
@@ -45,6 +59,56 @@ namespace Users.Controllers
 
             }
             return View(model);
+        }
+
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            AppUser user = await userManager.FindByIdAsync(id);
+
+            if (user != null)
+            {
+                return View(user);
+            }
+            else
+            {
+                return RedirectToAction("Index");
+            }
+        }
+
+
+        [HttpPost]
+        public async Task<IActionResult> Delete(string Id)
+        {
+            AppUser user = await userManager.FindByIdAsync(Id);
+
+            if (user != null)
+            {
+                IdentityResult result = await userManager.DeleteAsync(user);
+
+                if (result.Succeeded)
+                {
+                    return RedirectToAction("Index");
+                }
+                else
+                {
+                    AddErrorsFromResult(result);
+                }
+            }
+            else
+            {
+                ModelState.AddModelError("", "User Not Found");
+            }
+            return View("Index", userManager.Users);
+        }
+
+
+        private void AddErrorsFromResult(IdentityResult result)
+        {
+            foreach (IdentityError error in result.Errors)
+            {
+                ModelState.AddModelError("", error.Description);
+            }
         }
     }
 }
