49244afd97e36601c1cf5d61e8abe98d6ef52cb8
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-04 20:51:17 +0200

    Applied the authorize attribute.

--- Users/Controllers/HomeController.cs
+++ Users/Controllers/HomeController.cs
@@ -1,11 +1,13 @@
 ﻿using System;
 using System.Collections.Generic;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Authorization;
 
 namespace Users.Controllers
 {
     public class HomeController : Controller
     {
+        [Authorize]
         public ViewResult Index() => View(new Dictionary<string, object> { ["Placeholder"] = "Placeholder" });
     }
 }
