3091edc8d68dcae4cf7a6229d87b40fafec87a37
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-04 22:10:27 +0200

    Finished the Login HttpPost action method so the user will be able to sign in.

--- Users/Controllers/AccountController.cs
+++ Users/Controllers/AccountController.cs
@@ -1,5 +1,6 @@
 ﻿using System.Threading.Tasks;
 using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
 using Users.Models;
 
@@ -8,6 +9,15 @@ namespace Users.Controllers
     [Authorize]
     public class AccountController : Controller
     {
+        private UserManager<AppUser> _userManager;
+        private SignInManager<AppUser> _signInManager;
+
+        public AccountController(UserManager<AppUser> usrMgr, SignInManager<AppUser> signInMgr)
+        {
+            _userManager = usrMgr;
+            _signInManager = signInMgr;
+        }
+
         [AllowAnonymous]
         public IActionResult Login(string returnUrl)
         {
@@ -21,6 +31,27 @@ namespace Users.Controllers
         [ValidateAntiForgeryToken]
         public async Task<IActionResult> Login(LoginModel details, string returnUrl)
         {
+            if (ModelState.IsValid)
+            {
+                AppUser user = await _userManager.FindByEmailAsync(details.Email);
+
+                if (user!=null)
+                {
+                    await _signInManager.SignOutAsync();
+                    Microsoft.AspNetCore.Identity.SignInResult result = await _signInManager.PasswordSignInAsync(
+                            user,
+                            details.Password,
+                            false,
+                            false
+                        );
+
+                    if (result.Succeeded)
+                    {
+                        return Redirect(returnUrl ?? "/");
+                    }
+                }
+                ModelState.AddModelError(nameof(LoginModel.Email), "Invalid user or password");
+            }
             return View(details);
         }
     }
