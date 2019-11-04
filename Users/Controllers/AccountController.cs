d4ea192a9447ddb8a7d756f971b8dacfd40b586c
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-04 21:19:18 +0200

    Created AccountController.

---
+++ Users/Controllers/AccountController.cs
@@ -0,0 +1,26 @@
+﻿using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    public class AccountController : Controller
+    {
+        [AllowAnonymous]
+        public IActionResult Login(string returnUrl)
+        {
+            ViewBag.returnUrl = returnUrl;
+            return View();
+        }
+
+
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> Login(LoginModel details, string returnUrl)
+        {
+            return View(details);
+        }
+    }
+}
