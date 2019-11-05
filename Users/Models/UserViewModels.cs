5366a028ecf1ca582353d2707ce0429ab9102c20
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-05 23:49:46 +0200

    Added RoleModificationModel

--- Users/Models/UserViewModels.cs
+++ Users/Models/UserViewModels.cs
@@ -33,4 +33,13 @@ namespace Users.Models
         public IEnumerable<AppUser> Members { get; set; }
         public IEnumerable<AppUser> NonMembers { get; set; }
     }
+
+    public class RoleModificationModel
+    {
+        [Required]
+        public string RoleName { get; set; }
+        public string RoleId { get; set; }
+        public string[] IdsToAdd { get; set; }
+        public string[] IdsToDelete { get; set; }
+    }
 }
