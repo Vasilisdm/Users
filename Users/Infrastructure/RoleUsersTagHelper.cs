4c517cdc367e233cbe3a1ca309d66eeaa104f226
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-05 23:11:59 +0200

    Created the tag helper for identity-role attribute which will be used to display all the members of a certain role.

---
+++ Users/Infrastructure/RoleUsersTagHelper.cs
@@ -0,0 +1,45 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+using Users.Models;
+
+namespace Users.Infrastructure
+{
+    [HtmlTargetElement("td", Attributes = "identity-role")]
+    public class RoleUsersTagHelper : TagHelper
+    {
+        private UserManager<AppUser> _userManager;
+        private RoleManager<IdentityRole> _roleManager;
+
+        public RoleUsersTagHelper(UserManager<AppUser> usrMgr, RoleManager<IdentityRole> idtRole)
+        {
+            _userManager = usrMgr;
+            _roleManager = idtRole;
+        }
+
+        [HtmlAttributeName("identity-role")]
+        public string Role { get; set; }
+
+        public override async Task ProcessAsync(TagHelperContext context, TagHelperOutput output)
+        {
+            List<string> names = new List<string>();
+
+            IdentityRole role = await _roleManager.FindByIdAsync(Role);
+
+            if (role!=null)
+            {
+                foreach (var user in _userManager.Users)
+                {
+                    if (user!=null && await _userManager.IsInRoleAsync(user, role.Name))
+                    {
+                        names.Add(user.UserName);
+                    }
+                }
+            }
+
+            output.Content.SetContent(names.Count() == 0 ? "No users" : string.Join(", ", names));
+        }
+    }
+}
