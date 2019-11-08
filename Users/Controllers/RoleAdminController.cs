82d40e772a1ad57fb45485d30ae763cd001faa5f
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-08 19:58:25 +0200

    Implemented the edit method, and pass UserManager type as a dependency in the constructor.

--- Users/Controllers/RoleAdminController.cs
+++ Users/Controllers/RoleAdminController.cs
@@ -1,18 +1,22 @@
-﻿using System.ComponentModel.DataAnnotations;
+﻿using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Users.Models;
 
 namespace Users.Controllers
 {
     public class RoleAdminController : Controller
     {
         private RoleManager<IdentityRole> _roleManager;
+        private UserManager<AppUser> _userManager;
 
 
-        public RoleAdminController(RoleManager<IdentityRole> roleMgr)
+        public RoleAdminController(RoleManager<IdentityRole> roleMgr, UserManager<AppUser> usrMgr)
         {
             _roleManager = roleMgr;
+            _userManager = usrMgr;
         }
 
         public ViewResult Index() => View(_roleManager.Roles);
@@ -40,6 +44,27 @@ namespace Users.Controllers
             return View(name);
         }
 
+
+        public async Task<IActionResult> Edit(string id)
+        {
+            IdentityRole role = await _roleManager.FindByIdAsync(id);
+            List<AppUser> members = new List<AppUser>();
+            List<AppUser> nonMembers = new List<AppUser>();
+
+            foreach (AppUser user in _userManager.Users)
+            {
+                var list = await _userManager.IsInRoleAsync(user, role.Name) ? members : nonMembers;
+                list.Add(user);
+            }
+
+            return View(new RoleEditModel
+            {
+                Role = role,
+                Members = members,
+                NonMembers = nonMembers
+            });
+        }    
+    
 
         [HttpPost]
         public async Task<IActionResult> Delete(string id)
