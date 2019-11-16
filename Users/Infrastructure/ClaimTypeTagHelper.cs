e8cfceff2ea1c9c4660a0686e31105814253566a
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-16 13:02:25 +0200

    Implemented the tag helper, which is going to show the claims related to a certain user.

--- Users/Infrastructure/ClaimTypeTagHelper.cs
+++ Users/Infrastructure/ClaimTypeTagHelper.cs
@@ -1,10 +1,34 @@
-﻿using System;
+﻿using System.Linq;
+using System.Reflection;
+using System.Security.Claims;
+using Microsoft.AspNetCore.Razor.TagHelpers;
+
 namespace Users.Infrastructure
 {
-    public class ClaimTypeTagHelper
+    [HtmlTargetElement("td", Attributes = "identity-claim-type")]
+    public class ClaimTypeTagHelper : TagHelper
     {
-        public ClaimTypeTagHelper()
+        [HtmlAttributeName("identity-claim-type")]
+        public string ClaimType { get; set; }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
         {
+            bool foundType = false;
+            FieldInfo[] fields = typeof(ClaimTypes).GetFields();
+
+            foreach (FieldInfo field in fields)
+            {
+                if (field.GetValue(null).ToString() == ClaimType)
+                {
+                    output.Content.SetContent(ClaimType);
+                    foundType = true;
+                }
+            }
+
+            if (!foundType)
+            {
+                output.Content.SetContent(ClaimType.Split(',', '.').Last());
+            }
         }
     }
 }
