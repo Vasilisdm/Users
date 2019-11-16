6ecf0f7ad215cf0e73dd7bc9a132de6f4290b547
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-16 13:47:15 +0200

    Implemented the provider which, adds postal code constant to bob user and assigns the provinence.

--- Users/Infrastructure/LocationClaimsProvider.cs
+++ Users/Infrastructure/LocationClaimsProvider.cs
@@ -1,10 +1,32 @@
-﻿using System;
+﻿using System.Security.Claims;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Authentication;
+
 namespace Users.Infrastructure
 {
-    public class LocationClaimsProvider
+    public class LocationClaimsProvider : IClaimsTransformation
     {
-        public LocationClaimsProvider()
+        public Task<ClaimsPrincipal> TransformAsync(ClaimsPrincipal principal)
         {
+            if (principal!=null && !principal.HasClaim(c => c.Type == ClaimTypes.PostalCode))
+            {
+                ClaimsIdentity identity = principal.Identity as ClaimsIdentity;
+
+                if (identity!=null && identity.IsAuthenticated && identity.Name != null)
+                {
+                    if (identity.Name.ToLower() == "bob")
+                    {
+                        identity.AddClaims(new Claim[] {
+                            CreateClaim(ClaimTypes.PostalCode, "NY 10036"),
+                            CreateClaim(ClaimTypes.StateOrProvince, "NY")
+                        });
+                    }
+                }
+            }
+            return Task.FromResult(principal);
         }
+
+        private static Claim CreateClaim(string type, string value) =>
+            new Claim(type, value, ClaimValueTypes.String, "RemoteClaims");
     }
 }
