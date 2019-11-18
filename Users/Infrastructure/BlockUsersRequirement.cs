644949cc696e4a57dc6b4cc618f65ebe215652b4
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-18 21:04:09 +0200

    Implemented the BlockUsersRequirement which is going to be used as a class for custom requirements, on which I am going to add the handlers/subclasses.

---
+++ Users/Infrastructure/BlockUsersRequirement.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Authorization;
+
+namespace Users.Infrastructure
+{
+    public class BlockUsersRequirement : IAuthorizationRequirement
+    {
+        public string[] BlockedUsers { get; set; }
+
+        public BlockUsersRequirement(params string[] users)
+        {
+            BlockedUsers = users;
+        }
+    }
+}
