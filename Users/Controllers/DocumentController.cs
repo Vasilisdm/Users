496c7bf892ef9b73fcdc5824cd82a943f54cc11e
vasilisdimitriou <vasilisdmr@gmail.com>
2019-11-18 21:54:51 +0200

    Implemented the document controller which is going to render the docs array, display document if tha name is matched.

---
+++ Users/Controllers/DocumentController.cs
@@ -0,0 +1,23 @@
+﻿using System.Linq;
+using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+using Users.Models;
+
+namespace Users.Controllers
+{
+    [Authorize]
+    public class DocumentController : Controller
+    {
+        private ProtectedDocument[] docs = {
+           new ProtectedDocument { Title = "Q3 Budget", Author = "Alice", Editor = "Joe"},
+           new ProtectedDocument { Title = "Project Plan", Author = "Bob", Editor = "Alice"}
+        };
+
+        public ViewResult Index() => View(docs);
+
+        public ViewResult Edit(string title)
+        {
+            return View("Index", docs.FirstOrDefault(d => d.Title == title));
+        }
+    }
+}
